a0bd74f376e5bd099449dc20dec54f91cd10930b
Kirill Gavrilov <gavvvr@users.noreply.github.com>
2025-10-21 23:40:50 +0300

    simplify test

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,16 +1,11 @@
 import { expect, browser, $ } from '@wdio/globals'
 
 describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
+    it('should print web page title', async () => {
         await browser.url(`https://the-internet.herokuapp.com/login`)
-
-        await $('#username').setValue('tomsmith')
-        await $('#password').setValue('SuperSecretPassword!')
-        await $('button[type="submit"]').click()
-
-        await expect($('#flash')).toBeExisting()
-        await expect($('#flash')).toHaveText(
-            expect.stringContaining('You logged into a secure area!'))
+        
+        const title = await browser.getTitle()
+        console.log('Hello ' + title + ' website');
     })
 })
 
